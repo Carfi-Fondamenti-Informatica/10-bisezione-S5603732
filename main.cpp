ced1c3712ca07e9733c45a78e0f223be8becc1e2
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-12-22 19:26:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,25 +4,30 @@
 using namespace std;
 
 int main() {
-     float a, b, f, g, err, x, h;
+      float a, b, f, g, x, h, c, d;
     do{
         cout << "inserire estremi\n";
         cin >> a >> b;
+        if(a > b) {
+            c = a;
+            d = b;
+            a = d;
+            b = c;
+        }
         f = pow(a, 2) * cos(a) + 1;
         g = pow(b, 2) * cos(b) + 1;
     }while(f * g >= 0);
-    do {
-        a = (a + b) / 2;
-        if (f == 0) {
-            cout << a << endl;
-            cout << f;
-            return 0;
-        }else err = abs((b - a) / 2);
-    }while(err >= exp(1) - 6 && f != 0);
-    if(f * g < 0) b = x;
-    else a = x;
-    h = pow(x, 2) * cos(x) + 1;
-    cout << x << endl;
-    cout << h;
+   do{
+       x = (a + b) / 2;
+       h = pow(x, 2) * cos(x) + 1;
+       if((h < 0.001) && (h > -0.001)) {
+           cout << x << endl;
+           cout << h;
+       }else if(h > 0) {
+           b = x;
+       }else {
+           a = x;
+       }
+   }while((h >= 0.001) || (h <= -0.001));
    return 0;
 }
